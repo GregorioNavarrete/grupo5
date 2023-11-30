ab1e91910a35ede813475ecbf624d2e0509a0a2b
francoled <ledesmaf659@gmail.com>
2023-11-30 20:01:48 -0300

    inicio vistas dinamicas con ejs

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,10 @@
 const path = require('path');
-
+const product=require('../model/product');
 
 const productController = {
 
     index :  (req, res) => {
-        res.render('products/index');
+        res.render('products/index', product.getAllProducts());
       },
 
      cart:  (req, res) => {
