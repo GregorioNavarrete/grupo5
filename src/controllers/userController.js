1b28c23d56b9aaae74334be4a303b9c07bef3426
francoled <ledesmaf659@gmail.com>
2024-01-06 17:43:29 -0300

    multer-carga de usuarios-campos minimos agregados

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const fs = require('fs');
-const { log, Console } = require('console');
-const UsersFilePath= path.join(__dirname, '../data/users.json');
+const userService = require('../data/userService');
 
 const userController = {
 
-    Users:JSON.parse(fs.readFileSync(UsersFilePath, 'utf-8')),
+    
 
     login : (req, res) => {
         res.render('users/login');
@@ -15,69 +14,11 @@ const userController = {
         res.render('users/register');
       },
 
-      create: (req, res) => {
-        let archivoUsuario = fs.readFileSync(UsersFilePath, 'utf-8');
-      
-        // Verificar si el archivo está vacío
-        if (archivoUsuario == "") {
-          archivoUsuario = [];
-        } else {
-          // Convertir la cadena de texto en un array
-          archivoUsuario = JSON.parse(archivoUsuario);
-        }
-      
-        let ultimoId = archivoUsuario.reduce((maxId, usuario) => {
-          return usuario.id > maxId ? usuario.id : maxId;
-        }, archivoUsuario[0].id);
-      
-        let nuevoUsuario = {
-          id: ultimoId + 1,
-          nombre: req.body.nombre,
-          apellido: req.body.apellido,
-          usuario:req.body.usuario,
-          email: req.body.email,
-          contraseña: req.body.password,
-          categoria : "usuario",
-          imagen : "usuario.jpg"
-
-        }
-      
-        archivoUsuario.push(nuevoUsuario);
-        usuarioJSON = JSON.stringify(archivoUsuario);
-        fs.writeFileSync(UsersFilePath, usuarioJSON);
-      
-        res.redirect("/");
-      },
-
-
-
-      List: (req,res) => {
-        let Users = JSON.parse(fs.readFileSync(UsersFilePath, 'utf-8'));
-        res.render('users/userList', {Users:Users})
-    },
-
-
-
-    search: (req,res) => {
-      const Users = JSON.parse(fs.readFileSync(UsersFilePath, 'utf-8'));
-      let searchUsers = req.query.search.toLowerCase();
-      let Results = [];
-      for ( let i=0; i < Users.length;i++){
-         if(Users[i].nombre.toLowerCase().includes(searchUsers) || Users[i].apellido.toLowerCase().includes(searchUsers)){
-          Results.push(Users[i])
-        }
-      }
-      //res.send(Results)
-      res.render('users/userResults',{userResults : Results});
-    },
-
-
-    edit:(req,res)=>{
-      let Users = JSON.parse(fs.readFileSync(UsersFilePath, 'utf-8'));
-      let idUser= req.params.idUser;
-      let UserToEdit = Users[idUser];
-      res.render('userEdit',{UserToEdit:UserToEdit})
+    registrationProcess:(req,res)=>{
+      userService.create(req)
+      res.redirect('/');
     }
+      
 }
     
     
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,18 @@
 const express = require ('express');
 const router = express.Router();
 const userController = require ('../controllers/userController');
+const uploadUser = require('../middlewares/multerUser');
 
 router.get('/login', userController.login );
 router.get('/register', userController.registro);
-router.get('/admin/list', userController.List);
+router.get('/admin/list');
 
 
-router.get('/search',userController.search);//esto depende "/admin/list"
-router.get('/edit/:idUser',userController.edit);//esto depende "/admin/list"
+router.get('/search');//esto depende "/admin/list"
+router.get('/edit/:idUser');//esto depende "/admin/list"
 
 
-router.post('/register', userController.create);
+router.post('/register',uploadUser.single('img-user'), userController.registrationProcess);
 
 
 
