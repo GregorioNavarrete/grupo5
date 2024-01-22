2dae68ac535da4c57a5abff194f16ab7a963e75d
MaluO27 <marialourdesortega_27@hotmail.com>
2024-01-22 20:31:36 -0300

    CSS lista de usuario

--- src/middlewares/atenticaadmin.js
+++ src/middlewares/atenticaadmin.js
@@ -1,10 +1,8 @@
 function atenticaadmin(req, res, next) {
     //si usuario o no-usuario, no podran entra a paginas de admin 
-	console.log("estoy en autenticator");
 
 	if (req.session.userLogged != undefined){
 		if (req.session.userLogged.categoria === "usuario") {
-            console.log("no sos admin");
 			return res.redirect('/user/profile');
 		}
 	}
