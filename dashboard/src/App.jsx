cde236f95e9d2d0f38eb51f1dd3eb8776c0e8170
GregorioNavarrete <gregonavarrete30@gmail.com>
2024-03-24 19:32:24 -0300

    a

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import Home from './views/home.jsx';
 import About from './views/about.jsx';
 import Contact from './views/contact.jsx';
-import { Link , Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      
       <div>
+        
         <Link to='/'>Home</Link> <br/>
         <Link to='/about'>About</Link> <br/>
         <Link to='/contact'>Contact</Link> <br/>
         <Link to='/no-existe'>No existe</Link> <br/>
         <hr/>
-        
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
       </div>
     </>
   )
