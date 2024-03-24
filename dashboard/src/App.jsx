ea79ea2be549eb9a5c3c068ad1a8d2bf20f702c6
GregorioNavarrete <gregonavarrete30@gmail.com>
2024-03-24 20:22:17 -0300

    funciona las rutas y links de react

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
-import Home from './views/home.jsx';
-import About from './views/about.jsx';
-import Contact from './views/contact.jsx';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
+import Contact from './views/Contact';
 
 function App() {
   return (
-    <>
+    <Router>
       <div>
-        
         <Link to='/'>Home</Link> <br/>
         <Link to='/about'>About</Link> <br/>
         <Link to='/contact'>Contact</Link> <br/>
@@ -18,7 +17,7 @@ function App() {
         <Route path="/about" component={About}/>
         <Route path="/contact" component={Contact}/>
       </div>
-    </>
+    </Router>
   )
 }
 
